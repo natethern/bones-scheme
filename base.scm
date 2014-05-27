52721e82f36f45a0c33a1a51479464744b8d9762
felix <felix@call-with-current-continuation.org>
2014-05-27 13:46:54 +0200

    Add some default features in "base.scm" for x85_64/linux target.
    Also moved "structured.s" into x86_64 subdir.

--- base.scm
+++ base.scm
@@ -4,8 +4,13 @@
 
 (cond-expand
   (x86_64
-   (provide ieee754)
+   (provide flonums ieee754)
    (files "x86_64/intrinsics.scm")))
+
+(cond-expand
+  (linux 
+   (provide file-ports time jiffy-clock file-system process-environment))
+  (else))
 
 (files "r5rs.scm"
        "nonstd.scm")
