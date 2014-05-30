794b4ba081fe9307ecb94669e1df6099277f8e19
felix <felix@call-with-current-continuation.org>
2014-05-30 17:10:40 +0200

    Added MIPS case to base.scm.

--- base.scm
+++ base.scm
@@ -5,7 +5,9 @@
 (cond-expand
   (x86_64
    (provide flonums ieee754)
-   (files "x86_64/intrinsics.scm")))
+   (files "x86_64/intrinsics.scm"))
+  (mips
+   (files "mips/intrinsics.scm")))
 
 (cond-expand
   (linux 
