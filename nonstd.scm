46196a43d36c2688f0f5b61ab939d3932afe5a37
felix <felix@call-with-current-continuation.org>
2014-06-07 23:02:01 +0200

    Moved everything from r7rs.scm back into nonstd.scm.

--- nonstd.scm
+++ nonstd.scm
@@ -59,8 +59,118 @@
        (unless tmp (error args ...))
        tmp))))
 
+(define-syntax define-values
+  (syntax-rules ()
+    ((_ "1" () exp ((var tmp) ...))
+     (define
+       (call-with-values (lambda () exp)
+	 (lambda (tmp ...)
+	   (set! var tmp) ...))))
+    ((_ "1" (var . more) exp (binding ...))
+     (define-values "1" more exp (binding ... (var tmp))))
+    ((_ () exp) 
+     (define 
+       (call-with-values (lambda () exp)
+	 (lambda _ (void)))))
+    ((_ (var) exp) 
+     (define var exp))
+    ((_ (var ...) exp)
+     (begin
+       (define var #f) ...
+       (define-values "1" (var ...) exp ())))))
 
-;;XXX is this R7RS? extend, if needed
+
+(define open-input-string
+  (let ((substring substring))
+    (lambda (str)
+      (let ((data (cons (string-copy str) 0)))
+	(%make-port 
+	 #t #f 
+	 (lambda (p)
+	   (set-car! data "")
+	   (set-cdr! data 0))
+	 (lambda (p n)
+	   (let* ((p1 (cdr data))
+		  (s (car data))
+		  (len (string-length s))
+		  (p2 (%fx+ p1 n))
+		  (p2 (if (%fx<=? p2 len) p2 len)))
+	     (if (eq? p1 p2)
+		 (eof-object)
+		 (let ((r (substring s p1 p2)))
+		   (set-cdr! data p2)
+		   r))))
+	 data)))))
+
+(define open-output-string
+  (let ((make-string make-string))
+    (lambda ()
+      (let ((data (cons (make-string 1024) 0)))
+	(%make-port
+	 #f #f
+	 (lambda (p)
+	   (set-car! data "")
+	   (set-cdr! data 0))
+	 (lambda (p str)
+	   (let* ((p1 (cdr data))
+		  (s (car data))
+		  (n (string-length str))
+		  (len (string-length s))
+		  (p2 (%fx+ p1 n)))
+	     (when (%fx>? p2 len)
+	       (let ((new (make-string (%fx+ p2 5000))))
+		 ($inline "CALL copy_bytes" (cons s 0) (cons new 0) p1)
+		 (set! s new))
+	       (set-car! data s))
+	     (set-cdr! data p2)
+	     ($inline "CALL copy_bytes" (cons str 0) (cons s p1) n)
+	     str))
+	 data)))))
+
+(define get-output-string
+  (let ((substring substring))
+    (lambda (port)
+      (let* ((data (%slot-ref port 5))
+	     (str (car data))
+	     (p (cdr data)))
+	(substring str 0 p)))))
+
+
+(cond-expand
+  (time
+   (define-inline (current-second) ($inline "LIBCALL1 time, 0; INT2FIX rax")))
+  (else))
+
+
+(cond-expand
+  (process-environment
+
+   (define-inline (get-environment-variable str)
+     ($inline 
+      "CALL copy_to_buffer; LIBCALL1 getenv, buffer; test rax, rax; if z; mov rax, FALSE; endif; CALL alloc_zstring" 
+      str))
+
+   (define command-line
+     (let* ((argc (%argc))
+	    (lst (let loop ((i 0))
+		   (if (%fx>=? i argc)
+		       '()
+		       (cons (%argv-ref i) (loop (%fx+ i 1)))))))
+       (lambda () lst))))
+
+  (else))
+
+
+(cond-expand
+  (jiffy-clock
+   (define-inline (current-jiffy) ($inline "LIBCALL0 clock; INT2FIX rax"))
+   (define-inline (jiffies-per-second) 1000000))
+  (else))
+
+
+(define-syntax call/cc call-with-current-continuation)
+
+
 (define read-string
   (let ((make-string make-string)
 	(string-append string-append))
@@ -72,7 +182,6 @@
 	    (string-append (%char->string pc) str)
 	    str)))))
 
-;;XXX
 (define-syntax write-string
   (case-lambda
    ((str) ((%slot-ref %standard-output-port 3) %standard-output-port str))
@@ -83,12 +192,15 @@
 
    (define (current-directory . dir)
      (if (null? dir)
-	 ($inline "LIBCALL2 getcwd, buffer, 1024; mov rax, buffer; CALL alloc_zstring")	;XXX file-error
-	 (let ((r ($inline "CALL copy_to_buffer; LIBCALL1 chdir, buffer; INT2FIX rax" (car dir))))
-	   r)))				;XXX file-error
+	 (if (%fx<? ($inline "LIBCALL2 getcwd, buffer, 1024; mov rax, buffer; CALL alloc_zstring") 0)
+	     (%file-error 'current-directory)
+	     (let ((r ($inline "CALL copy_to_buffer; LIBCALL1 chdir, buffer; INT2FIX rax" (car dir))))
+	       (when (%fx<? r 0)
+		 (%file-error 'current-directory (car dir)))))))
 
    (define-inline (delete-file str) 
-     ($inline "CALL copy_to_buffer; LIBCALL1 unlink, buffer; INT2FIX rax" str))
+     (when (%fx<? ($inline "CALL copy_to_buffer; LIBCALL1 unlink, buffer; INT2FIX rax" str) 0)
+       (%file-error 'delete-file str)))
 
    (define-inline (file-exists? str)
      (and
@@ -116,8 +228,9 @@
    (define (open-append-output-file name)
      ;; open-flags: O_WRONLY|O_CREAT|O_APPEND, mode: S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH
      (let ((fd ($inline "CALL copy_to_buffer; LIBCALL3 open, buffer, 1089, 420; INT2FIX rax" name)))
-       ;;XXX check for error
-       (%make-file-output-port fd))))
+       (if (%fx<? fd 0)
+	   (%file-error 'open-append-output-file name)
+	   (%make-file-output-port fd)))))
   (else))
 
 
