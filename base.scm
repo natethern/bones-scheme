0aa37a5e33561233038a7c8015453e0e43b23b28
felix <felix@call-with-current-continuation.org>
2014-06-10 23:38:34 +0200

    Some cleanup in base.scm, also handle default configuration when none is given via -feature options.

--- base.scm
+++ base.scm
@@ -2,17 +2,22 @@
 
 (cond-expand
   (x86_64
-   (cond-expand 
-     ((not bare) (provide flonums ieee754))
-     (else))
-   (files "x86_64/intrinsics.scm")))
+   (files "x86_64/intrinsics.scm"))
+  (else (error "no architecture selected")))
 
 (cond-expand
-  (linux 
+  ((not (or linux linux-bare))
+   ;; map default-configuration to actual, if no specific target is given
+   (cond-expand
+     (default-linux (provide linux))))
+  (else))
+
+(cond-expand
+  ((or linux linux-bare)
    (provide file-ports file-system)
    (cond-expand
-     ((not bare)
-      (provide time jiffy-clock file-system process-environment))
+     (linux
+      (provide time jiffy-clock file-system process-environment flonums ieee754))
      (else)))
   (else))
 
