f633a10ba9c315c9c856c1549342eb9f3f51b9c9
felix <felix@call-with-current-continuation.org>
2014-06-15 23:16:54 +0200

    bones.h improvements.

--- bones.h
+++ bones.h
@@ -1,10 +1,20 @@
 /* bones.h - definitions for embedded programs compiled with BONES */
 
 
+#ifndef BONES_H
+#define BONES_H
+
+
+#ifdef __ILP64__
+typedef long long BONES_long;
+#else
+typedef long BONES_long;
+#endif
+
 typedef void *BONES_X;
 
 typedef struct BONES_block {
-  long header;
+  BONES_long header;
   BONES_X slots[ 1 ];
 } BONES_block;
 
@@ -15,10 +25,15 @@ typedef struct BONES_block {
 
 #define BONES_header_size(hdr)   ((hdr) & BONES_SIZE_MASK)
 #define BONES_header_type(hdr)   (((hdr) & BONES_TYPE_MASK) >> 56)
-#define BONES_fix2int(x)         ((long)(x) >> 1)
-#define BONES_int2fix(n)         ((BONES_X)(((long)(n) << 1) | 1))
+#define BONES_fix2int(x)         ((BONES_long)(x) >> 1)
+#define BONES_int2fix(n)         ((BONES_X)(((BONES_long)(n) << 1) | 1))
 
-#define BONES_isfixnum(x)        (((long)(x) & 1) != 0)
+#define BONES_slot_ref(x, i)     (((BONES_block *)(x))->slots[ i ])
+#define BONES_slot_set(x, i, y)  (((BONES_block *)(x))->slots[ i ] == (y))
+
+#define BONES_is_fixnum(x)       (((BONES_long)(x) & 1) != 0)
+#define BONES_size_of(x)         BONES_header_size(((BONES_block *)(x))->header)
+#define BONES_type_of(x)         BONES_header_type(((BONES_block *)(x))->header)
 
 #define BONES_NULL      0
 #define BONES_SYMBOL    1
@@ -35,4 +50,16 @@ typedef struct BONES_block {
 #define BONES_STRING    0x11
 #define BONES_CLOSURE   0x20
 
+#define BONES_ERROR_RECORD_ID   1
+
+#define BONES_is_error_object(x) \
+  (BONES_type_of(x) == BONES_RECORD && \
+   BONES_fix2int(BONES_slot_ref(x, 1)) == BONES_ERROR_RECORD_ID)
+
+#define BONES_string(x)         ((char *)(((BONES_block *)(x))->slots))
+#define BONES_float(x)          (*((double *)(((BONES_block *)(x))->slots)))
+
 extern BONES_X bones(BONES_X);
+
+
+#endif
