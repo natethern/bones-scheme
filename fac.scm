d0ddad2be22ea08dd169688a8eeadb59fe983cf2
felix <felix@call-with-current-continuation.org>
2014-06-23 12:34:30 +0200

    Removed program-spec from fac.scm.

--- fac.scm
+++ fac.scm
@@ -1,12 +1,7 @@
+(define (fac n)
+  (if (zero? n)
+      1
+      (* n (fac (- n 1)))))
 
-(program
- (include "base.scm")
- (code
-
-  (define (fac n)
-    (if (zero? n)
-	1
-	(* n (fac (- n 1)))))
-
-  (write-string (number->string (fac 10)))
-  (newline)))
+(write-string (number->string (fac 10)))
+(newline)
