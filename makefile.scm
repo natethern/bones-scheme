b091779a51687fe6fe94502d68a0c3bf0d06d9fa
felix <felix@call-with-current-continuation.org>
2014-06-26 21:31:17 +0200

    Add benchmark to show used heap in minimal program, added stripped executable sizes to benchmark log.

--- makefile.scm
+++ makefile.scm
@@ -141,14 +141,20 @@
   (run (git rev-parse HEAD >>benchmark.txt))
   (run (echo bones: >>benchmark.txt))
   (run (memtime ./bones compiler.scm -o /dev/null >>benchmark.txt 2>&1))
+  (run (strip bones ";" ls -l bones >>benchmark.txt))
   (run (echo dynamic: >>benchmark.txt))
   (run (./run dynamic.scm >>benchmark.txt 2>&1))
+  (run (strip dynamic ";" ls -l dynamic >>benchmark.txt))
   (run (echo mandelbrot: >>benchmark.txt))
   (run (./run mandelbrot.scm >>benchmark.txt 2>&1))
+  (run (strip mandelbrot ";" ls -l mandelbrot >>benchmark.txt))
   (run (echo fft: >>benchmark.txt))
   (run (./run fft.scm >>benchmark.txt 2>&1))
+  (run (strip fft ";" ls -l fft >>benchmark.txt))
+  (run (echo -n "'heap used: '" >>benchmark.txt))
+  (run (./run freeheap.scm >>benchmark.txt))
   (print "--------------------------------------------------------------------------------")
-  (run (tail -n 30 benchmark.txt)))
+  (run (tail -n 40 benchmark.txt)))
 
 (define distfiles
   '("MANUAL.txt"
