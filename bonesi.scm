867d2f79b1fa9e0195a66d3e120b57f8e7b0cc47
felix <felix@call-with-current-continuation.org>
2014-07-15 00:19:21 +0200

    Cleaned up bonesi.scm

--- bonesi.scm
+++ bonesi.scm
@@ -6,30 +6,12 @@
  (include "base.scm")
  (include "eval.scm")
 
+ (files "version.scm")
+
  (code
-  (do () (#f)
-    (display "> ")
-    (let ((x (read)))
-      (when (eof-object? x) (exit))
-      (call/cc
-       (lambda (return)
-	 (parameterize ((current-exception-handler
-			 (lambda (exn)
-			   (let ((out (current-error-port)))
-			     (newline out)
-			     (cond ((error-object? exn)
-				    (display "Error: " out)
-				    (display (error-object-message exn) out)
-				    (for-each
-				     (lambda (x)
-				       (newline out)
-				       (write x out)
-				       (newline out))
-				     (error-object-irritants exn)))
-				   (else
-				    (display "Unhandled excception: " out)
-				    (write exn out)
-				    (newline out)))
-			     (return #f)))))
-	   (write (eval x))
-	   (newline)))) ))) )
+
+  (let ((args (cdr (command-line))))
+    (cond ((null? args)
+	   (print "(BONES " bones-version ")")
+	   (repl))
+	  (else (load (car args))))) ) )
