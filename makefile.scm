184016a656d445bd24d7b5840e46aad4de3ec3f8
felix <felix@call-with-current-continuation.org>
2014-07-22 10:40:01 +0200

    Build embedded check using shared library.

--- makefile.scm
+++ makefile.scm
@@ -214,12 +214,16 @@
 
 (define (check-embedded)
   (bones)
-  (let ((r (and (zero? (run* (./bones tests/embedded.scm -o tmp/embedded.s -feature embedded)))
+  (let ((r (and (zero? (run* (./bones tests/embedded.scm -o tmp/embedded.s
+				      -feature pic -feature embedded)))
 		(zero? (run* (nasm -f ,nasm-format tmp/embedded.s 
 				   -o tmp/embedded1.o -DPREFIX=my)))
 		(zero? (run* (nasm -f ,nasm-format tmp/embedded.s
 				   -o tmp/embedded2.o -DPREFIX=my_other)))
-		(zero? (run* (gcc -g -I. tests/embedded.c tmp/embedded1.o tmp/embedded2.o -o tmp/embedded)))
+		(zero? (run* (gcc -g -I. tmp/embedded2.o -shared -o
+				  tmp/embedded2.so)))
+		(zero? (run* (gcc -g -I. tests/embedded.c tmp/embedded1.o 
+				  -o tmp/embedded -ldl)))
 		(zero? (run* (tmp/embedded))))))
     (unless r
       (print "embedding check failed."))
--- tests/embedded.c
+++ tests/embedded.c
@@ -3,15 +3,31 @@
 
 #include <stdio.h>
 #include <assert.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
+
 #include "bones.h"
 
 
 extern BONES_X my_bones(BONES_X);
-extern BONES_X my_other_bones(BONES_X);
+
+
+BONES_X (*my_other_bones)(BONES_X);
 
 
 int main(int argc, char *argv[])
 {
+#ifdef _WIN32
+  HMODULE h = LoadLibrary("tmp/embedded2.dll");
+  my_other_bones = ((BONES_X (*)(BONES_X))GetProcAddress(h, "my_other_bones");
+#else
+  void *h = dlopen("tmp/embedded2.so", RTLD_NOW);
+  my_other_bones = dlsym(h, "my_other_bones");
+#endif
   BONES_X x = my_bones(NULL);
   assert(BONES_fix2int(x) == 123);
   x = my_other_bones(NULL);
