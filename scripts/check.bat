d12ef4228f711fa3b6095e78a607e57ff458439e
felix <felix@call-with-current-continuation.org>
2014-07-22 11:29:37 +0200

    run run.bat in correct directory.

--- scripts/check.bat
+++ scripts/check.bat
@@ -4,9 +4,9 @@ set fail=0
 
 mkdir tmp
 
-for %%f in (tests\fac.scm tests\tak.scm tests\mandelbrot.scm tests\r4rstest.scm tests\r5rs_pitfalls.scm tests\dynamic.scm tests\compiler.scm tests\forth.scm) do echo %%f & call run %%f
+for %%f in (tests\fac.scm tests\tak.scm tests\mandelbrot.scm tests\r4rstest.scm tests\r5rs_pitfalls.scm tests\dynamic.scm tests\compiler.scm tests\forth.scm) do echo %%f & call scripts\run %%f
 
-call run tests\null.scm
+call scripts\run tests\null.scm
 dir tests\null
 
 echo.
