fb93aa535feccd5045f6d68c30240ac13d48320d
felix <felix@call-with-current-continuation.org>
2014-07-24 15:52:36 +0200

    Enhancements and bugfixes in bones.h

--- bones.h
+++ bones.h
@@ -24,25 +24,26 @@ typedef struct BONES_block {
 #define BONES_SPECIALBLOCK_BIT   0x2000000000000000
 
 /* basic types */
-#define BONES_NULL      0
-#define BONES_SYMBOL    1
-#define BONES_PAIR      2
-#define BONES_VECTOR    3
-#define BONES_CHAR      4
-#define BONES_EOF       5
-#define BONES_VOID      6
-#define BONES_BOOLEAN   7
-#define BONES_PORT      8
-#define BONES_PROMISE   9
-#define BONES_RECORD    10
-#define BONES_FLONUM    0x10
-#define BONES_STRING    0x11
-#define BONES_CLOSURE   0x20
+#define BONES_NULL      0L
+#define BONES_SYMBOL    1L
+#define BONES_PAIR      2L
+#define BONES_VECTOR    3L
+#define BONES_CHAR      4L
+#define BONES_EOF       5L
+#define BONES_VOID      6L
+#define BONES_BOOLEAN   7L
+#define BONES_PORT      8L
+#define BONES_PROMISE   9L
+#define BONES_RECORD    10L
+#define BONES_FLONUM    0x10L
+#define BONES_STRING    0x11L
+#define BONES_CLOSURE   0x20L
 
 #define BONES_ERROR_RECORD_ID   1
 
 /* access block header */
 #define BONES_header(x)          (((BONES_block *)(x))->header)
+#define BONES_header_set(x, h)   (((BONES_block *)(x))->header = h)
 
 /* operations on block-headers */
 #define BONES_header_size(hdr)   ((hdr) & BONES_SIZE_MASK)
@@ -58,15 +59,16 @@ typedef struct BONES_block {
 
 /* block data accessors */
 #define BONES_slot_ref(x, i)     (((BONES_block *)(x))->slots[ i ])
-#define BONES_slot_set(x, i, y)  (((BONES_block *)(x))->slots[ i ] == (y))
+#define BONES_slot_set(x, i, y)  (((BONES_block *)(x))->slots[ i ] = (y))
 
 /* these macros should not be used on a fixnum */
 #define BONES_size_of(x)         BONES_header_size(BONES_header(x))
 #define BONES_type_of(x)         BONES_header_type(BONES_header(x))
 
 #define BONES_is_error_object(x)					\
-  (BONES_type_of(x) == BONES_RECORD &&					\
-   BONES_fix2int(BONES_slot_ref((x), 1)) == BONES_ERROR_RECORD_ID)
+  ({BONES_X ___x = (x);							\
+    !BONES_is_fixnum(___x) && BONES_type_of(___x) == BONES_RECORD &&	\
+      BONES_fix2int(BONES_slot_ref(___x, 1)) == BONES_ERROR_RECORD_ID;})
 
 /* extract string-pointer and float-value */
 #define BONES_string(x)         ((char *)(((BONES_block *)(x))->slots))
