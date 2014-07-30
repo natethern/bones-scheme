f8fb59a1d045c5955bcbeb4a09d3c069b9221abd
felix <felix@call-with-current-continuation.org>
2014-07-31 00:38:35 +0200

    Added cond-expand to si.

--- eval.scm
+++ eval.scm
@@ -130,7 +130,35 @@
 			   (lambda (var) (k (lambda () handler)))))
 	     (call-with-values (lambda () body ...)
 	       (lambda results
-		 (k (lambda () (apply values results))))))))))))
+		 (k (lambda () (apply values results))))))))))
+     (define-syntax cond-expand
+       (syntax-rules (and or not else si)
+	 ((cond-expand) (error 'cond-expand "no matching clause"))
+	 ((cond-expand (si body ...) . more-clauses)
+	  (begin body ...))
+	 ((cond-expand (else body ...)) (begin body ...))
+	 ((cond-expand ((and) body ...) more-clauses ...) (begin body ...))
+	 ((cond-expand ((and req1 req2 ...) body ...) more-clauses ...)
+	  (cond-expand
+	    (req1 (cond-expand
+		    ((and req2 ...) body ...)
+		    more-clauses ...))
+	    more-clauses ...))
+	 ((cond-expand ((or) body ...) more-clauses ...) 
+	  (cond-expand more-clauses ...))
+	 ((cond-expand ((or req1 req2 ...) body ...) more-clauses ...)
+	  (cond-expand
+	    (req1 (begin body ...))
+	    (else
+	     (cond-expand
+	       ((or req2 ...) body ...)
+	       more-clauses ...))))
+	 ((cond-expand ((not req) body ...) more-clauses ...)
+	  (cond-expand
+	    (req (cond-expand more-clauses ...))
+	    (else body ...)))
+	 ((cond-expand (feature-id body ...) more-clauses ...)
+	  (cond-expand more-clauses ...))))))
 
 
 (define eval-unbound-value (list 'unbound))
