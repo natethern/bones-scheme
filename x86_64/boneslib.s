67be02cbf0c35a82285b98a13fe04a4f93e04821
felix <felix@call-with-current-continuation.org>
2014-08-04 16:46:47 +0200

    Use correct status tests for flonum-comparisons in "max" and "min" implementation.

--- x86_64/boneslib.s
+++ x86_64/boneslib.s
@@ -1218,7 +1218,7 @@ maximize_2:
   movsd xmm0, [rax + CELLS(1)]
 .fresult:
   ucomisd xmm0, xmm1
-  if l
+  if b
     movsd xmm0, xmm1
   endif
   movsd [temporary_flonum + CELLS(1)], xmm0
@@ -1256,7 +1256,7 @@ minimize_2:
   movsd xmm0, [rax + CELLS(1)]
 .fresult:
   ucomisd xmm0, xmm1
-  if g
+  if a
     movsd xmm0, xmm1
   endif
   movsd [temporary_flonum + CELLS(1)], xmm0
