42446edfb357665ebafdbbe8c98ca1e2e33db77e
felix <felix@call-with-current-continuation.org>
2014-09-09 11:36:52 +0200

    Trivial change in constants.scm

--- x86_64/linux/constants.scm
+++ x86_64/linux/constants.scm
@@ -7,12 +7,12 @@
 (define-syntax %O_CREAT 64)
 (define-syntax %O_TRUNC 512)
 (define-syntax %O_APPEND 1024)
-(define-syntax %WEXITED 4)
 (define-syntax %S_IRUSR 256)
 (define-syntax %S_IWUSR 128)
 (define-syntax %S_IRGRP 32)
 (define-syntax %S_IWGRP 16)
 (define-syntax %S_IROTH 4)
+(define-syntax %WEXITED 4)
 (define-syntax %CLOCK_MONOTONIC 1)
 ;; sigaction-size: 152
 ;; sigaction-handler-offset: 0
